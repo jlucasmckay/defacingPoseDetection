3b04332b9defece2cd678120fe1b13ad98487639
J. Lucas McKay PhD MSCR <j.lucas.mckay@gmail.com>
2024-07-11 16:40:33 -0400

    working

---
+++ mpTest2.py
@@ -0,0 +1,42 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+# For static images:
+IMAGE_FILES = ["FRONT.png"]
+BG_COLOR = (0, 0, 0) # black
+with mp_pose.Pose(
+    static_image_mode=True,
+    model_complexity=2,
+    enable_segmentation=True,
+    min_detection_confidence=0.5) as pose:
+  for idx, file in enumerate(IMAGE_FILES):
+    image = cv2.imread(file)
+    image_height, image_width, _ = image.shape
+    # Convert the BGR image to RGB before processing.
+    results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    if not results.pose_landmarks:
+      continue
+    print(
+        f'Nose coordinates: ('
+        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
+        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
+    )
+
+    # Create a black background image.
+    annotated_image = np.zeros(image.shape, dtype=np.uint8)
+    annotated_image[:] = BG_COLOR
+    
+    # Draw pose landmarks on the black background image.
+    mp_drawing.draw_landmarks(
+        annotated_image,
+        results.pose_landmarks,
+        mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+    
+    cv2.imwrite('output/annotated_image' + str(idx) + '.png', annotated_image)
